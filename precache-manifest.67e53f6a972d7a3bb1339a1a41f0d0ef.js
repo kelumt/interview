3a701a0c1410fb7cbda87c3669f70d33ac9d7b9c
Kelum Thenuwara <kelumt@users.noreply.github.com>
2020-03-31 08:17:12 +0530

    Updates

---
+++ precache-manifest.67e53f6a972d7a3bb1339a1a41f0d0ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3836f5ca29c4a201cb6b5fd54a577258",
+    "url": "/interview/index.html"
+  },
+  {
+    "revision": "91b14609a8a2e239d1e2",
+    "url": "/interview/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "326d5f694e3960edead8",
+    "url": "/interview/static/css/main.0fc37b51.chunk.css"
+  },
+  {
+    "revision": "91b14609a8a2e239d1e2",
+    "url": "/interview/static/js/2.2da49fc4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/interview/static/js/2.2da49fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326d5f694e3960edead8",
+    "url": "/interview/static/js/main.874621ea.chunk.js"
+  },
+  {
+    "revision": "1d2d0f7e6843e5fcace9",
+    "url": "/interview/static/js/runtime-main.ecee5237.js"
+  }
+]);
